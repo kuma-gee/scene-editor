605fbde9bdfb1c2345595a0b35a30977bc4193eb
myin <minyin142p@gmail.com>
2021-09-07 20:20:22 +0200

    Initial commit

---
+++ addons/scene-editor/plugin.gd
@@ -0,0 +1,27 @@
+tool
+extends EditorPlugin
+
+const DOCK_SCENE = preload("res://addons/scene-editor/SceneEditor.tscn")
+var dock
+
+func _ready():
+	connect("resource_saved", self, "_on_resource_saved")
+
+
+func _on_resource_saved(resource: Resource) -> void:
+	if dock:
+		dock.save()
+
+
+func _enter_tree():
+	dock = DOCK_SCENE.instance()
+	dock.interface = get_editor_interface()
+	add_control_to_bottom_panel(dock, "Scene Editor")
+	
+	# TODO: only add if not existing
+	add_autoload_singleton("SceneManager", "res://addons/scene-editor/SceneManager.gd")
+
+func _exit_tree():
+	remove_control_from_bottom_panel(dock)
+	dock.free()
+	
